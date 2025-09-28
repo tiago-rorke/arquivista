796ce6ba2b73ec4450743e0489cffa82c4f7a614
tiago-rorke <hello@tiago.nz>
2025-09-28 16:09:34 +0800

    remove MNAC proxy

--- start_chrome.sh
+++ start_chrome.sh
@@ -1,5 +1,5 @@
 # MNAC PROXY
-chromium-browser --proxy-server="10.110.16.7:8080" --proxy-bypass-list="localhost;127.0.0.1:8080"
+# chromium-browser --proxy-server="10.110.16.7:8080" --proxy-bypass-list="localhost;127.0.0.1:8080"
 
 # MODO NORMAL
-#chromium-browser
+chromium-browser
